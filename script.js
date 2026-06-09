1b2256f5fcbd77419075cb685c70d59f36788822
vgkansal123-ctrl <vgkansal123@gmail.com>
2026-06-09 15:33:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
-document.addEventListener('DOMContentLoaded',function () {
-console.log('Portfolio ของ SOMCHAI พร้อมแล้ว! ' );
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Portfolio ของ Kan พร้อมแล้ว!');
 });
